5588042ece4f8eadb78036a04f397cb870178808
Dhanusri366 <dhanusrigovindaraju@gmail.com>
2026-01-28 19:23:31 +0530

    Implement fast exponentiation method

---
+++ fastexponential.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public double myPow(double x, int n) {
+        long power = n;   // use long to avoid overflow
+
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        return fastPow(x, power);
+    }
+
+    private double fastPow(double x, long n) {
+        if (n == 0) return 1.0;
+
+        double half = fastPow(x, n / 2);
+
+        if (n % 2 == 0)
+            return half * half;
+        else
+            return half * half * x;
+    }
+}
